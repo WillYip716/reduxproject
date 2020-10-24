23fb5616c7d77090ae42cfdaa703267dccdf9184
WillYip716 <william.yip.716@gmail.com>
2020-10-23 17:52:09 -0700

    react files

--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true })
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('DB error', err))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
